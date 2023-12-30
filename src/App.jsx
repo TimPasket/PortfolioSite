5d1999dd62478d8dbdf9a06453099e04ba305243
TimPasket <tim.d.pasket@gmail.com>
2023-12-29 22:15:53 -0600

    inital

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Hello There!</h1>
+      <div className="card">
+        <h2>Stay tuned! An awesome site is in the making 😉</h2>
+        <p>
+          In the mean time, you can check out ongoing progress on GitHub or
+          explore my LinkedIn profile
+        </p>
+      </div>
+      <div>
+        <a href="https://www.linkedin.com/in/tim-pasket/">
+          <img src="/src/assets/linkedIn-icon-better.png" alt="linkedInIcon" />
+        </a>
+        <a href="https://github.com/TimPasket">
+          <img
+            className="gitIcon"
+            src="/src/assets/github-icon-best.png"
+            alt="githubIcon"
+          />
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default App;
